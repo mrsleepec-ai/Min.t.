3238973d809395dd32a6ba1bfa83ef6ba9fce6a5
mrsleepec-ai <mrsleepec@gmail.com>
2025-09-06 14:22:09 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SW v35
-const CACHE='minimal-tasks-cache-v35';
+const CACHE='minimal-tasks-cache-v36';
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const ks=await caches.keys(); await Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)));})()); self.clients.claim();});
